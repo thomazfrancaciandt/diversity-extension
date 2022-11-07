8de4ad3e9602c787d7686a490564acf5b3ee6ae0
alepereiraciandt <alexandrepereira@ciandt.com>
2022-11-07 14:03:18 -0300

    added function to revieve msg from background

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 console.log('load after screen load, inspect chrome to see this log');
+
+chrome.runtime.onMessage.addListener((msg, sender, resp) => {
+  console.log(msg);
+
+  if (msg.message === "palavra indevida")
+    confirm(`${msg.message} eh uma palavra indevida.`);
+});
