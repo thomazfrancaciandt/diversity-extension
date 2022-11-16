b4dafd495dabb11e51ab5832491ce6e9903344c9
alepereiraciandt <alexandrepereira@ciandt.com>
2022-11-16 07:58:58 -0300

    refac suggestions to suggestion

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((payload, sender, resp) => {
 
-  const {explanation, suggestions, trm } = payload;
+  const {explanation, suggestion, trm } = payload;
   
   alert(`"${trm}"
 Algumas palavras, frases e expressões carregam uma conotação discriminatória. A frase ou expressão utilizada no seu texto pode ser substituida e vai ajudar a moldar um vocabulario mais inclusivo, que tal nos ajudar a mudar esse cenario e utilizar as expressões:
-${suggestions} no lugar?`)
+${suggestion} no lugar?`)
 
 });
